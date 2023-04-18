f8fd38e575710e09d63048d76fb3112b49e15d23
root <root@Loaner01.providencepubliclibrary.org>
2023-04-18 14:46:12 -0400

    initial commit

---
+++ test.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby -w
+
+puts "Hello, Ruby!";
+
+# Member Functions in Ruby Class
+
+class Sample
+    def hello
+        puts "Hello Ruby!"
+    end
+end
+
+# Now using above class to create objects
+object = Sample. new
+object.hello
+
+# Ruby Class Case Study
