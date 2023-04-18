462df6fe02f734a0f7058f2953b9b7dac0f45ba4
root <root@Loaner01.providencepubliclibrary.org>
2023-04-18 14:52:17 -0400

    create the customer class

--- test.rb
+++ test.rb
@@ -15,3 +15,24 @@ object = Sample. new
 object.hello
 
 # Ruby Class Case Study
+# For your case study, you will create a Ruby Class called Customer and you will delcare two methods
+# display_details - This method will display the details of the customer
+# total_no_of_customers - This method will display the total number of customers created in the system
+
+class Customer
+    @@no_of_customers = 0
+    def initiatilize(id, name, addr)
+        @cust_id = id
+        @cust_name = name
+        @cust_addr = addr
+    end
+    def display_details()
+        puts "Customer id #@cust_id"
+        puts "Customer name #@cust_name"
+        puts "Customer address #@cust_addr"
+    end
+    def total_no_of_customers()
+        @@no_of_customers += 1
+        puts "Total number of customers: #@@no_of_customers"
+    end
+end
